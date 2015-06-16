f9bc2299c07499aa18b4b7134c8524d3ad268c3a
Sanjay Patel <sanjay.b.patel@capgemini.com>
2015-06-16 10:42:15 +0530

    Assignment1 Committed

---
+++ Assignment1/src/Awesome.java
@@ -0,0 +1,17 @@
+
+public class Awesome {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Awesome Coders....");
+		int i=1,sum=0;
+		if(i<=10)
+		{
+			sum = sum+i;
+			i++;
+		}
+		System.out.println("Sum is ="+i);
+
+	}
+
+}
